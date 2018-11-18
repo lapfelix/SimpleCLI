d871afa2c5b1309f0b3bc4ec9bcd332f512fff8b
Felix Lapalme <felix@lapal.me>
2018-11-18 16:12:07 -0500

    Make it work and set up basic tests

--- Sources/SimpleCLI/SimpleCLI.swift
+++ Sources/SimpleCLI/SimpleCLI.swift
@@ -1,3 +1,166 @@
-struct SimpleCLI {
-    var text = "Hello, World!"
+//
+//  CLIUtilities.swift
+//  SimpleCLI
+//
+//  Created by Felix Lapalme on 2018-11-18.
+//  Copyright © 2018 Felix Lapalme. All rights reserved.
+//
+
+import Foundation
+
+enum ArgumentType {
+    case keyOnly
+    case keyAndValue
+    case valueOnly
+}
+
+struct Argument {
+    let longName: String
+    let shortName: String?
+    let type: ArgumentType
+    let defaultValue: String?
+    let obligatory: Bool?
+
+    init(longName: String,
+         shortName: String? = nil,
+         type: ArgumentType = .keyAndValue,
+         defaultValue: String? = nil,
+         obligatory: Bool? = false) {
+        self.longName = longName
+        self.shortName = shortName
+        self.type = type
+        self.defaultValue = defaultValue
+        self.obligatory = obligatory
+    }
+}
+
+class SimpleCLI {
+    enum ProcessingError: Error {
+        case requiredKeyHasNoValue(key: String)
+        case unexpectedValueWithoutKey(value: String)
+        case unexpectedArgument(key: String)
+        case unknownArgument(key: String)
+    }
+
+    let configuration : [Argument]
+    let singleValueArgument : Argument?
+
+    init(configuration config: [Argument]) {
+        let singleValueArgumentArray = config.filter({$0.type == .valueOnly})
+        if (singleValueArgumentArray.count > 1) {
+            print("Error: Too many single value arguments")
+        }
+        
+        singleValueArgument = singleValueArgumentArray.first
+        
+        configuration = config
+    }
+
+    func parseArgs(_ args: [String]) -> Dictionary<String, String>? {
+        do {
+            return try parseArguments(args)
+        } catch {
+            // print the automatically generated help
+            print(error)
+            return [:]
+        }
+    }
+    
+    func parseArguments(_ args: [String]) throws -> Dictionary<String, String>? {
+        var dictionary : Dictionary<String, String> = [:]
+        var currentArgument : Argument?
+
+        for arg in args.dropFirst() {
+            let isLongKey = arg.starts(with: "--")
+            let isShortKey = !isLongKey && arg.starts(with: "-")
+
+            var newArgument: Argument?
+            
+            if isLongKey || isShortKey {
+                guard let argument = configuration.first(where: {
+                        if (isLongKey) {
+                            let key = arg.dropFirst(2)
+                            if ($0.longName == key) {
+                                return true
+                            }
+                        } else if (isShortKey) {
+                            guard let shortName = $0.shortName else {
+                                return false
+                            }
+
+                            let key = arg.dropFirst(1)
+                            if (shortName == key) {
+                                return true
+                            }
+                        }
+
+                        return false
+                    })
+                else {
+                    throw ProcessingError.unknownArgument(key: arg)
+                }
+
+                newArgument = argument
+
+                if let currentArgument = currentArgument {
+                    // We have a previous argument with no value. Let's process it.
+                    do {
+                        if let value = try process(argument: currentArgument) {
+                            dictionary[currentArgument.longName] = value
+                        }
+                    }
+                    catch {
+                        throw error
+                    }
+                }
+            }
+            else if let currentArgument = currentArgument {
+                // it's a value
+                do {
+                    if let value = try process(argument: currentArgument) {
+                        dictionary[currentArgument.longName] = value
+                    }
+                }
+                catch {
+                    throw error
+                }
+            }
+            else if let singleValueArgument = singleValueArgument {
+                if (dictionary[singleValueArgument.longName] == nil) {
+                    dictionary[singleValueArgument.longName] = arg
+                }
+                else {
+                    throw ProcessingError.unexpectedValueWithoutKey(value: arg)
+                }
+            }
+            else {
+                throw ProcessingError.unexpectedValueWithoutKey(value: arg)
+            }
+
+            currentArgument = newArgument
+        }
+
+        return dictionary
+    }
+
+    func process(argument: Argument, value: String? = nil) throws -> String? {
+        if let value = value {
+            return value
+        }
+
+        if (argument.type == .keyOnly) {
+            return String(true)
+        }
+
+        if let defaultValue = argument.defaultValue {
+            return defaultValue
+        }
+
+        if (argument.obligatory ?? false) {
+            throw ProcessingError.requiredKeyHasNoValue(key: argument.longName)
+        }
+        else {
+            return nil
+        }
+    }
 }
--- Tests/SimpleCLITests/SimpleCLITests.swift
+++ Tests/SimpleCLITests/SimpleCLITests.swift
@@ -2,14 +2,41 @@ import XCTest
 @testable import SimpleCLI
 
 final class SimpleCLITests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(SimpleCLI().text, "Hello, World!")
+    func testSimpleArg() {
+        let simpleCLI = SimpleCLI(configuration: [Argument(longName: "argument1", shortName: "A", type: .keyAndValue, defaultValue: "yes", obligatory: false)])
+        let parsed = simpleCLI.parseArgs(["executablepath", "--argument1", "yes"])
+        XCTAssertEqual(parsed, ["argument1":"yes"])
+    }
+
+    func testTwoArgs() {
+        let simpleCLI = SimpleCLI(configuration: [Argument(longName: "argument1", shortName: "A", type: .keyAndValue, defaultValue: "yes", obligatory: false)])
+        let parsed = simpleCLI.parseArgs(["executablepath", "oh", "no"])
+        XCTAssertEqual(parsed, [:])
+    }
+
+    func testSingleValueArgSuccess() {
+        let simpleCLI = SimpleCLI(configuration: [Argument.init(longName: "valueOnlyArgument", type: .valueOnly)])
+        let parsed = simpleCLI.parseArgs(["executablepath", "oh"])
+        XCTAssertEqual(parsed, ["valueOnlyArgument":"oh"])
+    }
+
+    func testSingleValueArgFailure() {
+        let simpleCLI = SimpleCLI(configuration: [Argument.init(longName: "valueOnlyArgument", type: .valueOnly)])
+        let parsed = simpleCLI.parseArgs(["executablepath", "oh", "no"])
+        XCTAssertEqual(parsed, [:])
+    }
+
+    func testNoConfigFailure() {
+        let simpleCLI = SimpleCLI(configuration: [])
+        let parsed = simpleCLI.parseArgs(["executablepath", "oh", "no"])
+        XCTAssertEqual(parsed, [:])
     }
 
     static var allTests = [
-        ("testExample", testExample),
+        ("testSimpleArg", testSimpleArg),
+        ("testTwoArgs", testTwoArgs),
+        ("testSingleValueArgSuccess", testSingleValueArgSuccess),
+        ("testSingleValueArgFailure", testSingleValueArgFailure),
+        ("testNoConfigFailure", testNoConfigFailure),
     ]
 }
